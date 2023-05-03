f27b95c25f75775665bc38b0c71244b4f2fc80a5
ninachen22 <116048270+ninachen22@users.noreply.github.com>
2023-05-03 18:36:17 -0400

    added for ifs

--- jsprobs.js
+++ jsprobs.js
@@ -1,9 +1,29 @@
 // Malware Analysis
-
+// X is compared to T
+// T, _, _, X, _, _, _, T
+// If X is smaller or equal to either T's, 
+// X is replaced with 0
 function simulate(entries) {
     // Write your code here
-    
-    return [];
+    for (let index = 0; index < entries.length; index++) {
+        var x = entries[index];
+        var t1 = entries[index - 3];
+        var t2 = entries[index + 4];
+        if (t1.index >= 0 && t2.index < entries.length) {
+            if (x <= t1 || x <= t2) {
+                entries[index] = 0;
+            }
+        } if (t1.index < 0 && t2.index < entries.length) {
+            if (x <= t2) {
+                entries[index] = 0;
+            }
+        } if (t1.index >= 0 && t2.index >= entries.length) {
+            if (x <= t1) {
+                entries[index] = 0;
+            }
+        }
+    }
+    return entries;
   }
   
   const records = [19, 2, 0, 87, 1, 40, 80, 77, 77, 77, 77];
